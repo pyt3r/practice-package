6149dc7da34fea6d0f5ffe7df9ef380ad6927a69
pyt3r <pyt3r.b@gmail.com>
2020-06-01 22:35:26 -0400

    adding partial solution

--- practice/probability/sock_drawer.py
+++ practice/probability/sock_drawer.py
@@ -17,13 +17,61 @@ Problem:
     (b) How small if the number of black socks is even?
 
 Solution:
-  This is a test for mathematical notation.
+  1. Let's define the probability of drawing red after 1 draw as
 
-  .. math::
+      .. math:: P(red_1) = \\frac{N_{red}}{N_{total}},
 
-     (a + b)^2 = a^2 + 2ab + b^2
+      where
 
-     (a - b)^2 = a^2 - 2ab + b^2
+      .. math:: N_{total} = N_{red} + N_{black}
+
+
+  2. Let's define the probability of drawing 2 consecutive reds as
+
+      .. math::
+
+        P(red_2|red_1) = P(red_1) *
+          \\frac{N_{red} - 1}{N_{total} - 1},
+
+      which can be expanded to
+
+      .. math::
+
+        = \\frac{N_{red}}{N_{total}} *
+          \\frac{N_{red} - 1}{N_{total} - 1}
+
+
+  3. Using the `binomial coefficient (combination)`_ equation,
+
+.. _binomial coefficient (combination): https://en.wikipedia.org/wiki/Combination
+
+      .. math::
+
+        \\binom{n}{k} = \\frac{n(n-1) ... (n-k+1)}{k(k-1) ... 1},
+
+      the probability of drawing 2 consecutive reds may be generalized as,
+
+      .. math::
+
+        P(red_2|red_1) = \\frac{\\binom{N_{red}}{2}}{\\binom{N_{Total}}{2}},
+
+      and ultimately,
+
+        = \\frac{\\binom{N_{red}}{2}}{\\binom{N_{red} + N_{black}}{2}}
+
+  In Code:
 
 
 """
+
+
+class Solution:
+
+    @staticmethod
+    def a(n_red: int, n_black: int, n_draws: int) -> float:
+        from itertools import combinations
+        n_total = n_red + n_black
+        pr = combinations(n_red, n_draws)
+        pr /= combinations(n_total, n_draws)
+        return pr
+# %%
